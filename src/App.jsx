1e4bd03cb5fe2e8820c092a3fb2aa7f8327a3f5e
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-14 11:47:50 +0530

    Navlink concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, NavLink, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Cart } from './pages/Cart'
 
 
 function App() {
+
+  const getStyles = ({isActive}) => {
+    return {
+      color: isActive ? 'blue' : 'black',
+      textDecoration: 'none',
+      fontSize: '20px',
+      margin: '10px'
+    }
+  }
+
   return (
     <div className='App'>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/cart">Cart</Link>
+      <NavLink style={getStyles} to="/">Home</NavLink>
+      <NavLink style={getStyles} to="/about">About</NavLink>
+      <NavLink style={getStyles} to="/cart">Cart</NavLink>
 
       <Routes>
         <Route path='/'element={<Home />} />
